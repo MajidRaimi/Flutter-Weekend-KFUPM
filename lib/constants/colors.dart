f213ca26c9db9a065f40c3449c8d033202d97502
MajidRaimi <imajid2000z1@gmil.com>
2022-11-18 14:16:52 +0300

    Setup

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kBlueColor = Color(0xFF36a3c8);
+const kGreenColor = Color(0xFF7dc144);
+const kGrayColor = Color(0xFF7dc144);
+const kWhiteColor = Color(0xFFFEFEFE); 
