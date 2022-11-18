dc6a7ef6e677f80e4543aa8adf327b7e9c304d21
MajidRaimi <imajid2000z1@gmil.com>
2022-11-18 19:02:36 +0300

    Problem Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'screens/login_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,16 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('I Am Poor'),
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/coal.png'),
-          ),
-        ),
-      ),
+      home: LoginScreen(),
     );
   }
 }
